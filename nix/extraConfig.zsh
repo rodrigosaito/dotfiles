69d665be291a751b64297be42941b879b216000c
Rodrigo Saito <rodrigo.saito@gmail.com>
2020-10-26 14:09:09 +0000

    new tmux conf and other stuff

--- nix/extraConfig.zsh
+++ nix/extraConfig.zsh
@@ -2,7 +2,12 @@
 bindkey '^[^[[D' emacs-backward-word
 bindkey '^[^[[C' emacs-forward-word
 
+if [ -e /home/ubuntu/.nix-profile/etc/profile.d/nix.sh ]; then . /home/ubuntu/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+
 [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
 
-for file in /Users/rodrigosaito/src/github.com/Shopify/cloudplatform/workflow-utils/*.bash; do source ${file}; done
-kubectl-short-aliases
+[ -d /Users/rodrigosaito/src/github.com/Shopify/cloudplatform ] && for file in /Users/rodrigosaito/src/github.com/Shopify/cloudplatform/workflow-utils/*.bash; do source ${file}; done
+
+if type kubectl-short-aliases > /dev/null; then
+  kubectl-short-aliases
+fi
